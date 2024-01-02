448c0b662495d29a92b174eda63ca7df7e40a441
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-01-02 23:47:58 +0530

    id route crated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -39,6 +39,14 @@ app.get('/service', async(req, res)=>{
     const cursor = serviceCollection.find(query);
     const result = await cursor.toArray();
     res.send(result);
+})
+
+app.get('/services/:id', async(req, res)=>{
+  const id = req.params.id;
+  console.log(id)
+  const query = {_id: new ObjectId(id)};
+  const service = await serviceCollection.findOne(query);
+  res.send(service);
 })
 
   } finally {
